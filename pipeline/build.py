051016bbabae7b23c91489b558c1074547a27e16
dumeng <du.mang@malgo.cn>
2019-10-20 11:28:46 +0800

    add config

--- pipeline/build.py
+++ pipeline/build.py
@@ -9,14 +9,18 @@ from typing import ClassVar
 def build_data_action(config):
     def action_func(config, inputs):
         # return a dict object, should be saved as json file
-        return {'data': {'name': 'dumeng', 'title': 'frontend engineer'}}
+        return {'data': {'name': 'dumeng', 'title': 'frontend engineer', 'meta': config['a']}}
     return action_func
 
 
 def post_build_data_action(config):
     def action_func(config, inputs):
+        # now inputs is a dict, because it is saved as a json file from the `build data` action
+        data = inputs['data']
+        name = data['name']
+        n = int(data['meta'])
         # return a Iterable, should be saved as a .pkl file
-        l = [f'line {x} \n' for x in range(10)]
+        l = [f'{name} {x} \n' for x in range(n)]
         return {'post_data': l}
     return action_func
 
--- pipeline/process.py
+++ pipeline/process.py
@@ -6,21 +6,20 @@ from abc import ABC
 from typing import ClassVar
 
 
-def build_data_action(config):
+def process_data_action(config):
     def action_func(config, inputs):
-        # return a dict object, should be saved as json file
-        return {'data': {'name': 'dumeng', 'title': 'frontend engineer'}}
+        # inputs is now a Map[string][Generator], because it is saved as `pkl` file from 'post_build_data`
+        gene = inputs['post_data']
+        env = config['env']
+        lines = []
+        for l in gene():
+            lines.append(l)
+        lines.insert(
+            0, 'I am the line inserted by `process_data_action`: {}'.format(env))
+        # and return a model
+        return {'processed_data': lines}
     return action_func
 
 
-def post_build_data_action(config):
-    def action_func(config, inputs):
-        # return a Iterable, should be saved as a .pkl file
-        l = [f'line {x} \n' for x in range(10)]
-        return {'post_data': l}
-    return action_func
-
-
-class BuildDataComponent(ABC):
-    ACTION_DICT: ClassVar = {'build_data': build_data_action,
-                             'post_build_data': post_build_data_action}
+class ProcessDataComponent(ABC):
+    ACTION_DICT: ClassVar = {'process_data': process_data_action}
